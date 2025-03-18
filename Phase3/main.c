01e5be4472c00ac018756e5f194ae9bddaaf7799
Muzna <91617542+MSMALG@users.noreply.github.com>
2025-03-19 02:00:49 +0300

    Update main.c

--- Phase3/main.c
+++ Phase3/main.c
@@ -1,11 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "global.h"
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char *argv[]) {
-	init();
-	parse();
-	return 0;
-	
+int main() {
+    output = fopen("output.obj", "w");
+    err = fopen("file.err", "w");
+    input = fopen("file.exp", "r");  // Open user-provided input file
+
+    if (!output) {
+        perror("Error opening output file");
+        return 1;
+    }
+    if (!err) {
+        perror("Error opening error file");
+        return 1;
+    }
+    if (!input) {
+        perror("Error opening input file");
+        return 1;
+    }
+
+    //printf("before\n");
+
+    //Read the first character from the input file
+    int t = getc(input);
+    //printf("eee");
+    
+    if (t == EOF) {
+        fprintf(err, "Error: Input file is empty or invalid\n");
+        return 1;
+    }
+    
+    //Push the character back to the stream
+    ungetc(t, input);
+
+    printf("Infix expression read from input file\n");
+
+    init();
+    parse();
+
+    fclose(output);
+    fclose(err);
+    fclose(input);
+
+    return 0;
 }
