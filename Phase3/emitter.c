ce094439ce7ca90f1ec48394cb496ba2300cbb96
Muzna <91617542+MSMALG@users.noreply.github.com>
2025-03-19 01:51:32 +0300

    Update emitter.c

--- Phase3/emitter.c
+++ Phase3/emitter.c
@@ -5,27 +5,30 @@ extern struct entry symtable[];
 void emit(int t, int tval)
 {
 	switch(t) {
-	case '+': printf("\npush r1\npop r2\nadd r2, r1\npush r2"); break;
-    case '-': printf("\npush r1\npop r2\nsub r2, r1\npush r2"); break;
-    case '*': printf("\npush r1\npop r2\nmult r2, r1\npush r2"); break;
-    case '/': printf("\npush r1\npop r2\ndiv r2, r1\npush r2"); break;
+	//case '+': printf("\npush r1\npop r2\nadd r2, r1\npush r2"); break;
+	//or 
+	case '+': fprintf(output,"\npop r1\npop r2\nadd r2, r1\npush r2\n"); break; //do it to other cases as well.
+    case '-': fprintf(output,"\npop r1\npop r2\nsub r2, r1\npush r2\n"); break;
+    case '*': fprintf(output,"\npop r1\npop r2\nmult r2, r1\npush r2\n"); break;
+    case '/': fprintf(output,"\npop r1\npop r2\ndiv r2, r1\npush r2\n"); break;
 	//	printf("%c", t); 
-		break;
+		//break;
 	case DIV:
-		printf("DIV");
+		//printf("DIV");
+		fprintf(output,"\npop r1\npop r2\ndiv r2, r1\npush r2\n"); break;
 		break;
 
 	case MOD:
-		printf("MOD"); 
+        fprintf(output,"\npop r1\npop r2\nmod r2, r1\npush r2\n"); break;
 		break;
 	case NUM:
-		printf("%d", tval);
+		//printf("%d", tval);
+		fprintf(output, "push %d\n", tval); //in al cases replace with fprintf
 		break;
 	case ID:
-		printf("%s", symtable[tval].lexptr); 
+		fprintf(output ,"%s", symtable[tval].lexptr); 
 		break;
 	default:
-		printf("token %d, tokenval %d\n", t, tval);
+		fprintf(output,"token %d, tokenval %d\n", t, tval);
 	}
 }
-
