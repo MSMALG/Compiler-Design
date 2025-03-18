a9b1ed6cd621ab61178cfff27b9c6beff59698cf
Muzna <91617542+MSMALG@users.noreply.github.com>
2025-03-19 01:52:04 +0300

    Update global.h

--- Phase3/global.h
+++ Phase3/global.h
@@ -1,3 +1,6 @@
+#ifndef GLOBAL_H
+#define GLOBAL_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -14,22 +17,25 @@
 #define DONE   260
 
 int tokenval;
+int lexan();
+void parse();
+void expr();
+void term();
+void factor();
+void match(int t);
+void emit(int t, int tval);
+void init();
+void error(char* m);
+int lookup(char s[]);
+int insert(char s[], int tok);
 
 int lineno;
+int lookahead;
+FILE *output, *err, *input;
 
 struct entry {
    char *lexptr;
    int token;
    };
 
-   int lexan();
-   void parse();
-   void expr();
-   void term();
-   void factor();
-   void match(int t);
-   void emit(int t, int tval);
-   void init();
-   void error(char* m);
-   int lookup(char s[]);
-   int insert(char s[], int tok);
+#endif
